802f8311ddeaa7b6cf6e11b2163ec5dd243d59ee
Jason Willis <jawillis72@gmail.com>
2022-06-22 12:10:05 -0500

    Added e46 config

---
+++ configs/e46.h
@@ -0,0 +1,76 @@
+// Customizations for BMW e46
+// (1999-2005 model years).
+
+#if !defined(RACECHRONO_BIY_BLE_DEVICE_E46_H)
+#define RACECHRONO_BIY_BLE_DEVICE_E46_H)
+
+uint8_t getUpdateRateDivider(uint32_t can_id) {
+  // This is sent over the CAN bus 140 times per second and carries:
+  // vehicle speed
+  // Let's go for ~28 per second
+  if (can_id == 0x153) {
+    return 5;
+  }
+
+  // This is sent over the CAN bus 140 times per second and carries:
+  // individual wheel speeds 
+  // we want ~28.
+  if (can_id == 0x1F0) {
+    return 5;
+  }
+
+  // This is sent over the CAN bus 100 times per second and carries:
+  // engine RPM 
+  // we want ~25.
+  if (can_id == 0x1F0) {
+    return 4;
+  }
+
+  // This is sent over the CAN bus 100 times per second and carries:
+  // Steering angle 
+  // we want ~25.
+  if (can_id == 0x1F5) {
+    return 4;
+  }
+
+  // This is sent over the CAN bus 100 times per second and carries:
+  // Coolant temp
+  // Ambient pressure
+  // Throttle Position
+  // we want ~25.
+  if (can_id == 0x329) {
+    return 4;
+  }
+
+  // This is sent over the CAN bus 5 times per second and carries:
+  // oil temp 
+  // we want 5.
+  if (can_id == 0x545) {
+    return 1;
+  }
+
+  // This is sent over the CAN bus 5 times per second and carries:
+  // odometer
+  // fuel level 
+  // we want ~5.
+  if (can_id == 0x613) {
+    return 1;
+  }
+
+  // This is sent over the CAN bus 5 times per second and carries:
+  // ambient air temp 
+  // we want ~5.
+  if (can_id == 0x615) {
+    return 1;
+  }
+
+  // OBD responses should be rare, don't limit them at all if we're listening to
+  // them.
+  if (can_id > 0x700) {
+    return 1;
+  }
+
+  return DEFAULT_UPDATE_RATE_DIVIDER;
+}
+
+#endif // RACECHRONO_BIY_BLE_DEVICE_E46_H
