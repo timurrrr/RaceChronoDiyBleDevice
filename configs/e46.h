82dcf8b1ad0259200b3d543dfbd8238b63031c41
Jason Willis <jawillis72@gmail.com>
2022-07-01 02:42:10 -0500

    Add e46 config and wiki page

--- configs/e46.h
+++ configs/e46.h
@@ -7,61 +7,61 @@
 uint8_t getUpdateRateDivider(uint32_t can_id) {
   // This is sent over the CAN bus 140 times per second and carries:
   // vehicle speed
-  // Let's go for ~28 per second
+  // Let's go for ~10 per second
   if (can_id == 0x153) {
-    return 5;
+    return 14;
   }
 
   // This is sent over the CAN bus 140 times per second and carries:
   // individual wheel speeds 
-  // we want ~28.
+  // we want ~10.
   if (can_id == 0x1F0) {
-    return 5;
+    return 14;
   }
 
   // This is sent over the CAN bus 100 times per second and carries:
   // engine RPM 
-  // we want ~25.
+  // we want ~10.
   if (can_id == 0x1F0) {
-    return 4;
+    return 10;
   }
 
   // This is sent over the CAN bus 100 times per second and carries:
   // Steering angle 
-  // we want ~25.
+  // we want ~10.
   if (can_id == 0x1F5) {
-    return 4;
+    return 10;
   }
 
   // This is sent over the CAN bus 100 times per second and carries:
   // Coolant temp
   // Ambient pressure
   // Throttle Position
-  // we want ~25.
+  // we want ~10.
   if (can_id == 0x329) {
-    return 4;
+    return 10;
   }
 
-  // This is sent over the CAN bus 5 times per second and carries:
+  // This is sent over the CAN bus 100 times per second and carries:
   // oil temp 
-  // we want 5.
+  // we want ~1.
   if (can_id == 0x545) {
-    return 1;
+    return 100;
   }
 
   // This is sent over the CAN bus 5 times per second and carries:
   // odometer
   // fuel level 
-  // we want ~5.
+  // we want ~1.
   if (can_id == 0x613) {
-    return 1;
+    return 5;
   }
 
   // This is sent over the CAN bus 5 times per second and carries:
   // ambient air temp 
-  // we want ~5.
+  // we want ~1.
   if (can_id == 0x615) {
-    return 1;
+    return 5;
   }
 
   // OBD responses should be rare, don't limit them at all if we're listening to
